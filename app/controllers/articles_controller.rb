dc07e90ed68476d0e4cb8c7fc791189a2237470e
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-03-08 16:14:20 -0800

    added edit, update, show, and index articles paths and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -16,6 +24,16 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was succesfully updated"
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
     end
 
     private
